e3bdcb2eb2ffc79e98bb581ef1541108fd607d10
unknown <greateducation123@yahoo.com>
2022-12-09 15:24:33 -0600

    combining the lists

--- Day_Trip.py
+++ Day_Trip.py
@@ -3,3 +3,11 @@ restaurants = ["La Cabana", "Burgers and More", "Fresh Garden", "Garcia's"]
 transportation = ["car", "plane", "train", "helicopter"]
 entertainment = ["circus", "boat rides", "parachuting", "hiking"]
 
+day_trip = ["destinations", "restaurants", "transportation", "entertainment"]
+
+
+
+
+
+
+# random.sample()
