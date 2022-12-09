7e99d54e5e7b0d0c8b645fc1803195b501fc4b1b
unknown <greateducation123@yahoo.com>
2022-12-09 16:15:18 -0600

    more notes

--- Day_Trip.py
+++ Day_Trip.py
@@ -12,9 +12,11 @@ day_trip = ["destinations", "restaurants", "transportation", "entertainment"]
 
 # random.sample()
 # Are you satisfied with your trip? Y or N
-# Which option would you like to change?
+# Which option would you like to change? distinations, restaurants, transportation, entertainment
+# Here is your final trip!
 
 # def run_day_trip_generator():
+
 # def print_full_trip(list_of_options):
 # def generate_random_item(list_of_items):
 # def determine_satisfaction(current_trip, trip_options):
@@ -23,3 +25,7 @@ day_trip = ["destinations", "restaurants", "transportation", "entertainment"]
 
 # run_day_trip_generator()
 
+# random.sample(list,k) where k represents, number of values to be sampled.
+# list_1 = [1,3,5]
+
+# final_list = random.sample(list_1,1)+random.sample(list_2,1)+random.sample(list_3,1)
