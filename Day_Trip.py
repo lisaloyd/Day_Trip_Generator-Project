dd042f64273041e00a6a58065453d6b7fb915894
unknown <greateducation123@yahoo.com>
2022-12-09 14:59:04 -0600

    The entertainment list is complete

--- Day_Trip.py
+++ Day_Trip.py
@@ -1,5 +1,5 @@
 destinations = ["Pensacola", "Cyprus", "Colorado Springs", "Dallas"]
 restaurants = ["La Cabana", "Burgers and More", "Fresh Garden", "Garcia's"]
 transportation = ["car", "plane", "train", "helicopter"]
-
+entertainment = ["circus", "boat rides", "parachuting", "hiking"]
 
